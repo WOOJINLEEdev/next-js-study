20900fc1ac4976fd5536e9186de4dd8b06981f5f
WOOJINLEEdev <lwj3794@naver.com>
2022-11-27 23:39:21 +0900

    next auth 코드 수정

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import FacebookProvider from "next-auth/providers/facebook";
+import NaverProvider from "next-auth/providers/naver";
+import KakaoProvider from "next-auth/providers/kakao";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "lib/prisma";
@@ -9,6 +11,12 @@ import { getEnv } from "utils/env";
 
 const googleClientId = getEnv("GOOGLE_CLIENT_ID") || "";
 const googleClientSecret = getEnv("GOOGLE_CLIENT_SECRET") || "";
+
+const naverClientId = getEnv("NAVER_CLIENT_ID") || "";
+const naverClientSecret = getEnv("NAVER_CLIENT_SECRET") || "";
+
+const kakaoClientId = getEnv("KAKAO_CLIENT_ID") || "";
+const kakaoClientSecret = getEnv("KAKAO_CLIENT_SECRET") || "";
 
 const githubClientId = getEnv("GITHUB_ID") || "";
 const githubClientSecret = getEnv("GITHUB_SECRET") || "";
@@ -21,6 +29,14 @@ export const authOptions = {
     GoogleProvider({
       clientId: googleClientId,
       clientSecret: googleClientSecret,
+    }),
+    NaverProvider({
+      clientId: naverClientId,
+      clientSecret: naverClientSecret,
+    }),
+    KakaoProvider({
+      clientId: kakaoClientId,
+      clientSecret: kakaoClientSecret,
     }),
     GitHubProvider({
       clientId: githubClientId,
